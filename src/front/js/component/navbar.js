8bc2b15994795172b6e4886475fc663a32c21b32
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2022-08-13 22:38:44 +0000

    :new: Vanta and Navigation hook created

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useScrollPosition } from "../hooks/useScrollPosition";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  const scrollPosition = useScrollPosition();
+
+  return (
+    <nav
+      className={`w-full h-12 flex transparent fixed
+        ${scrollPosition > 200 ? "bg-white/20 backdrop-blur-sm" : "transparent"}
+        `}
+      style={{ transition: "all 1s ease-out" }}
+    >
+      <div className="flex justify-start h-full w-full">
+        <p className="my-auto mx-8 text-white font-black">
+          My New Project {scrollPosition}
+        </p>
+      </div>
+      <div className="flex justify-end justify-items-end h-full w-full">
+        <button className="bg-teal-600 rounded-md align-middle mx-8 my-2 px-4 text-white font-bold">
+          Login
+        </button>
+      </div>
+    </nav>
+  );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,50 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  const { store, actions } = useContext(Context);
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        VANTA.TRUNK({
+          el: "#home",
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          spacing: 7.0,
+          chaos: 7.0,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+  return (
+    <>
+      <div className="h-full z-[-10]" id="home">
+        <div className="flex h-full w-full items-center justify-center container mx-auto px-8">
+          <div className="max-w-4xl text-center">
+            <h1 className="text-3xl sm:text-5xl capitalize tracking-widest text-white font-black lg:text-7xl">
+              Start Crafting Your
+            </h1>
+            <span
+              class="relative mt-2 md:inline-block text-3xl "
+              style={{ color: "#98465f" }}
+            >
+              Next Great Path
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="h-96"></div>
+    </>
+  );
 };
